1971a31d335a0231d6a82815b47be98807a92721
Adam Brewer <adamcbrewer@gmail.com>
2012-07-16 06:50:32 +0100

    Almost all correct types of data now being parsed.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,89 @@ $(function () {
 	// Load running CSV's
 	//
 	var ds = new Miso.Dataset({
-		url : "/csv/Activities.01.csv",
+		url : "/activities.all.csv",
 		delimiter : ";",
-		sync: true
+		columns: [
+			{
+				name: 'favourite',
+				type: 'string'
+			},
+			{
+				name: 'name',
+				type: 'string'
+			},
+			{
+				name: 'activity',
+				type: 'string'
+			},
+			{
+				name: 'course',
+				type: 'string'
+			},
+			{
+				name: 'date',
+				type: 'time',
+				before: function (value) {
+					return moment(value);
+				}
+			},
+			{
+				name: 'duration',
+				type: 'number',
+				before: function (duration) {
+
+					var result = null;
+
+					if (duration) {
+						var hms = duration.split(':'),
+							units = hms.length,
+							seconds = 0;
+
+						for (var i = 0; i < units; i++) {
+							hms[i] = parseInt(hms[i], 10);
+						}
+
+						if (units === 3) {
+							seconds = (hms[0] * 60 * 60) + (hms[1] * 60) + hms[2];
+						} else {
+							seconds = (hms[0] * 60) + hms[1];
+						}
+
+						result = seconds;
+					}
+					return result;
+				}
+			},
+			{
+				name: 'distance',
+				type: 'number'
+			},
+			{
+				name: 'elevation gain',
+				type: 'number'
+			},
+			{
+				name: 'average pace',
+				type: 'mixed'
+			},
+			{
+				name: 'max pace',
+				type: 'mixed'
+			},
+			{
+				name: 'calories',
+				type: 'number',
+				before: function (value) {
+					return (value) ? parseInt(value.replace(/,/g, ''), 10) : null;
+				}
+			}
+		]
+		// sync: true
 	});
 
 	ds.fetch({
 		success: function() {
-			
+
 			var columnNames = this.columnNames(),
 				rows = this.length,
 				data = this,
@@ -18,24 +93,24 @@ $(function () {
 
 			columnNames.forEach(function (el, i) {
 				col = data.column(el);
-				// console.log(col.name + ' - ' + col.type);
+				console.log(col.name + ' - ' + col.type);
 			});
 
 			// console.log(this.sum('Distance'));
 
-			this.sum('Distance').bind('change', function () {
-				console.log('The sum has changed to: ' + this.sum());
-			});
+			// this.sum('Distance').bind('change', function () {
+			// 	console.log('The sum has changed to: ' + this.sum());
+			// });
 
-			var maxDist = this.max('Distance');
+			// var maxDist = this.max('Distance');
 			
-			var row = this.where({
-				column: 'Distance',
-				rows: function (row) {
-					return row.Distance == maxDist;
-				}
-			});
-			console.log(row);
+			// var row = this.where({
+			// 	column: 'Distance',
+			// 	rows: function (row) {
+			// 		return row.Distance == maxDist;
+			// 	}
+			// });
+			console.log(this.rows());
 			
 		}
 	});
--- index.php
+++ index.php
@@ -99,7 +99,7 @@
 	<script src="assets/js/libs/moment.js"></script>
 	<script src="assets/js/libs/miso.ds.0.2.1.js"></script>
 	
-	<script src="assets/js/script.min.js"></script>
+	<script src="assets/js/script.js"></script>
 	
 </body>
 </html>
